ca1746b64384daa2bf64699a8fdf32de801d4f75
David Alfter <alfter.david@gmx.net>
2015-06-22 14:24:46 +0200

    Created cached lemmatizer.

---
+++ src/de/unitrier/daalft/pali/cache/CachedLemmatizer.java
@@ -0,0 +1,38 @@
+package de.unitrier.daalft.pali.cache;
+
+import java.util.List;
+
+import de.general.log.ILogInterface;
+import de.unitrier.daalft.pali.morphology.Lemmatizer;
+import de.unitrier.daalft.pali.morphology.element.ConstructedWord;
+import de.unitrier.daalft.pali.morphology.paradigm.ParadigmAccessor;
+
+public class CachedLemmatizer {
+
+	private Cache<List<ConstructedWord>> cache;
+	private Lemmatizer lemmatizer;
+	
+	public CachedLemmatizer(ParadigmAccessor pa, int maxCacheSize, int maxCacheDurationInSeconds) throws Exception {
+		cache = new Cache<List<ConstructedWord>>(maxCacheSize, maxCacheDurationInSeconds);
+		lemmatizer = new Lemmatizer(pa);
+	}
+		
+	public boolean lemmaExists(String lemma) {
+		return (cache.get(lemma, false) != null);
+	}
+
+	public List<ConstructedWord> lemmatize (String word, String pos) throws Exception {
+		List<ConstructedWord> lemmata = cache.get(word, false);
+		if (lemmata != null) {
+			return lemmata;
+		}
+		lemmata = lemmatizer.lemmatize(null, word, pos);
+		cache.put(word, lemmata);
+		return lemmata;
+	}
+
+	public String lemmatizeWithDictionary(ILogInterface log, String word,
+			String pos) throws Exception {
+		return lemmatizer.lemmatizeWithDictionary(log, word, pos);
+	}
+}
